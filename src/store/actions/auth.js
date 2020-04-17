eda00293940cf143befb5ef37ff7777811dbdadc
unknown <mohamed.hassan2498@gmail.com>
2020-04-17 23:12:34 +0200

    initial commit

---
+++ src/store/actions/auth.js
@@ -0,0 +1,48 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart()); 
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url =  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDPyaV_F_zZh25mUZlv1bsi9GYmghcU-4g';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDPyaV_F_zZh25mUZlv1bsi9GYmghcU-4g';
+        }
+        axios.post(url, authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail(err));
+
+        })
+    };
+};
